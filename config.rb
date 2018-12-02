b64b60f82e0d5b8910843f4d0c4df741bb42ccfe
celiopires <celiopires@icloud.com>
2018-12-02 17:02:02 +0100

    Fix new directory

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 http_path = "/"
 css_dir = "assets/css"
 sass_dir = "assets/scss"
-images_dir = "assets/img"
+assets/images_dir = "assets/img"
 javascripts_dir = "assets/js"
 relative_assets = true
 line_comments = true
