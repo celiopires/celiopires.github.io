e657f73c5ed02acc1ec6cfca9e656cabd50cbb9b
celiopires <celiopires@icloud.com>
2018-12-04 23:06:06 +0100

    Fix script

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -12,42 +12,59 @@ function yScroll() {
 window.addEventListener("scroll", yScroll);
 
 // Starting the fun
-var letters = [];
-var letter = "A";
+var bubbles = [];
 
 function setup() {
     var cnv = createCanvas(innerWidth, innerHeight-100);
     cnv.id('heroShowcase');
     cnv.class('hero');
-    cnv.style('position','absolute'); 
-    cnv.style('top', '0');
-    cnv.style('right', '0');
-    cnv.style('left', '0');
+    // cnv.style('position','absolute'); 
+    // cnv.style('top', '0');
+    // cnv.style('right', '0');
+    // cnv.style('left', '0');
     cnv.parent('heroStand');
-  for (var i = 0; i<30; i++){
-    letters.push(new Letters());
-  }
+    for (var i = 0; i<30; i++){
+      bubbles.push(new Bubbles());
+    }
 }
 function draw() {
   background(255);
-  for (var i=0; i<letters.length; i++){
-    letters[i].display();
-    letters[i].move();
+  for (var i=0; i<20; i++){
+    bubbles[i].shape1();
+    bubbles[i].move();
   }
 }
 
-function Letters() {
+function Bubbles() {
   this.x = random(windowWidth);
   this.y = random(windowHeight);
-  this.speed = 0.5;
-  
+  this.speed = 0.2;
+
   this.move = function() {
-    this.x += random(-this.speed, this.speed);
-    this.y += random(-this.speed, this.speed);
+    this.x += random(-this.speed*1.4, this.speed);
+    this.y += random(-this.speed*0.4, this.speed);
   };
-  
-  this.display = function() {
-    fill(150);
-    text(letter, this.x, this.y, 100, 100);
+
+  this.shape1 = function() {
+    push();
+      translate(100,100);
+      stroke(0,0,0,40);
+      strokeWeight(1);
+      fill(0,0,0,0);
+      ellipse(this.x, this.y, 80, 80);
+    pop();
+    
+    push();
+      translate(50,50);
+      stroke(0,0,0,30);
+      strokeWeight(1);
+      fill(0,0,0,0);
+      rect(this.x, this.y, 40, 40);
+    pop();
+    
+    stroke(0,0,0,20);
+    strokeWeight(1);
+    fill(0,0,0,0);
+    rect(this.x, this.y, 20, 20);
   };
 }
