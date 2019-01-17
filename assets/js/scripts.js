477279b38277e4f9347420d2c7e50f7ccdff1cda
celiopires <celiopires@icloud.com>
2019-01-17 21:22:49 +0100

    Hotfix

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -15,18 +15,16 @@ window.addEventListener("scroll", yScroll);
 var bubbles = [];
 
 function setup() {
+    colorMode(RGB);
     var cnv = createCanvas(innerWidth, innerHeight-100);
     cnv.id('heroShowcase');
     cnv.class('hero');
-    // cnv.style('position','absolute'); 
-    // cnv.style('top', '0');
-    // cnv.style('right', '0');
-    // cnv.style('left', '0');
     cnv.parent('heroStand');
     for (var i = 0; i<30; i++){
       bubbles.push(new Bubbles());
     }
 }
+
 function draw() {
   background(255);
   for (var i=0; i<20; i++){
@@ -48,23 +46,23 @@ function Bubbles() {
   this.shape1 = function() {
     push();
       translate(100,100);
-      stroke(0,0,0,40);
+      stroke(0,0,0,20);
       strokeWeight(1);
-      fill(0,0,0,0);
-      ellipse(this.x, this.y, 80, 80);
+      fill(255,255,255);
+      ellipse(this.x, this.y, 100, 100);
     pop();
     
     push();
       translate(50,50);
-      stroke(0,0,0,30);
+      stroke(0,0,0,10);
       strokeWeight(1);
-      fill(0,0,0,0);
-      rect(this.x, this.y, 40, 40);
+      fill(255,255,255);
+      rect(this.x*0.2, this.y, 40, 40);
     pop();
     
-    stroke(0,0,0,20);
+    stroke(0,0,0,40);
     strokeWeight(1);
-    fill(0,0,0,0);
+    fill(255,255,255);
     rect(this.x, this.y, 20, 20);
   };
 }
