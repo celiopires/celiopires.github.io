6755626745cefdcc61450dbdfeccadb52ffb3d38
Célio <celiopires@icloud.com>
2022-08-04 10:53:05 +0100

    Setting up automations

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+// gulp
+var gulp        = require('gulp'),
+    watch       = require('gulp-watch'),
+    plumber     = require('gulp-plumber'),
+    cleanCSS    = require('gulp-clean-css'),
+    sourcemaps  = require('gulp-sourcemaps'),
+    sass        = require('gulp-sass')(require('sass'));
+
+// watch
+gulp.task('watch', function() {
+    gulp.watch('assets/scss/**/*.scss', gulp.series('sass'));
+});
+
+// css
+gulp.task('sass', function() {
+    return gulp.src('assets/scss/**/*.scss')
+    .pipe(plumber())
+    .pipe(sourcemaps.init())
+    .pipe(sass.sync({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest('assets/css'));
+});
+
+// CSS MINIFY
+gulp.task('minify-css', function() {
+    return gulp.src('assets/css/*.css')
+    //.pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    //.pipe(sourcemaps.write())
+    .pipe(rename('styles.min.css'))
+    .pipe(gulp.dest('assets/css/'));
+  });
+
+// Default Task
+//gulp.task('default', ['sass', 'watch', 'rev', 'revRewrite']);
+//gulp.task('default', ['watch', 'sass']);
+gulp.task('default', gulp.series('watch'));
+
+
+
