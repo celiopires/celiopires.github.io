58b49c56a8044e00977a997a69431ef16e2d7f34
Célio <celiopires@icloud.com>
2022-10-30 23:36:31 +0000

    Worked on the scroll interactions

---
+++ assets/js/scripts.js
@@ -0,0 +1,22 @@
+window.scrollTo({
+  top: 500,
+  behavior: 'smooth'
+});
+
+
+// var anchor = document.querySelector('#intro__header');
+
+// setTimeout(function () {
+//   // anchor.scrollTo(500, 0);
+// 	anchor.scrollTo({
+// 		top: 500,
+// 		left: 100,
+// 		behavior: 'smooth'
+// 	});
+// }, 300);
+
+// window.scrollTo({
+//   top: 500,
+//   left: 100,
+//   behavior: 'smooth'
+// });
